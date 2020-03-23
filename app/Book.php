707b91d775a7a24623c78b0bbce575fc121a12b8
Eric Omanoff <eric.omanoff@gmail.com>
2020-03-23 08:55:30 -0600

    updated model and controller

--- app/Book.php
+++ app/Book.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $fillable = ['title', 'author', 'book_list_id', 'num_pages', 'order'];
+    
+    public function booklist()
+    {
+        return $this->belongsTo(BookList::class);
+    }
 }
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,23 +11,13 @@ class BookController extends Controller
     public function store(Request $request, $id)
       {
 
-        $validatedData = $request->validate([
+        $request->validate([
           'title' => 'required',
           'author' => 'required'
         ]);
-
-        $booklist = BookList::find($id);
-        $listSize = $booklist->books->count();
-
-        $book = Book::create([
-          'book_list_id' => $id,
-          'title' => $validatedData['title'],
-          'author' => $validatedData['author'],
-          'num_pages' => $request->input('num_pages', null),
-          'rating' => $rating->input('rating', null),
-          'order' => $listSize
-        ]);
-
+        $booklist = BookList::findOrFail($id);
+        $book = Book::create(array_merge($request->all(), ['order' => $booklist->books->count()]));
+        $book->booklist()->associate($booklist);
         return response(201)->json($book);
       }
 
